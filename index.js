b507e6e8e3a44adac7e2e8cd288bcb1514f666b3
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-02-17 14:38:15 -0800

    commit tree db04f3b9308ce206c68aca28e9486e3ec93a7fcd

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.querySelector("h1#victory");
+let name = "Natasha";
+newHeader.innerHTML = `${name} is the champion`;
